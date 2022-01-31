01e9e90f7d9fd862b3e97641ea7275d3ad8f44ae
sy.choi <sy.choi@salin.co.kr>
2022-02-01 02:18:40 +0900

    javascript notes

---
+++ operator.js
@@ -0,0 +1,183 @@
+// 1. String concatenation(연쇄)
+console.log("my" + " cat");
+console.log("1" + 2); // 12
+console.log(`string literals: 1+2 = ${1 + 2}`);
+console.log("effy's book"); // 따옴표가 중복될때
+
+// Escape Sequence : 문자열에서 표현하기 어려운 문자를 백슬래쉬(\)를 통해 표현
+// '\n' : 줄바꿈(Enter)
+// '\t' : 탭(tab)
+// '\' : backspace
+
+// 2. Numeric operators
+console.log(1 + 1); // add
+console.log(1 - 1); // substract
+console.log(1 / 1); // divide
+console.log(1 * 1); // multiply
+console.log(1 % 1); // remainder
+console.log(1 ** 1); // exponentiation
+
+// 3. Increment and decrement operators (++ 증가/ -- 감소)
+let iCounter = 2;
+const preIncrement = ++iCounter;
+// counter = counter + 1;
+// preIncrement = counter;
+console.log(`preIncrement: ${preIncrement}, counter: ${iCounter}`);
+const postIncrement = iCounter++;
+// postIncrement = counter;
+// counter = counter + 1;
+console.log(`postIncrement: ${postIncrement}, counter: ${iCounter}`);
+let dCounter = 2;
+const preDecrement = --dCounter;
+console.log(`postIncrement: ${preDecrement}, counter: ${dCounter}`);
+const postDecrement = dCounter--;
+console.log(`postIncrement: ${postDecrement}, counter: ${dCounter}`);
+
+// 4. Assingment operators
+let x = 3;
+let y = 6;
+x += y; // x = x + y
+x -= y; // x = x - y
+x *= y; // x = x * y
+x /= y; // x = x / y
+
+// 5. Comparison operators
+console.log(10 < 6); // less than
+console.log(10 <= 6); // less than or equal
+console.log(10 > 6); // greater than
+console.log(10 >= 6); // greater than or equal
+
+// 6. Logical operators : ||(or), &&(and), !(not)
+const value1 = true;
+const value2 = 4 < 2;
+
+// ||(or), finds the first truthy value
+console.log(`or: ${value1 || value2 || check()}`);
+
+// &&(and), finds the first falsy value
+console.log(`and: ${value1 && value2 && check()}`);
+
+// [TIP] Heavy Operation 일수록 뒤에 작성하기
+function check() {
+  for (let i = 0; i < 10; i++) {
+    //wasting time
+    console.log("😅");
+  }
+  return true;
+}
+
+// !(not)
+console.log(!value1);
+
+// 7. Equality
+const stringFive = "5";
+const numberFive = 5;
+
+// '==' loose eqaulity, with type conversion
+console.log(stringFive == numberFive);
+console.log(stringFive != numberFive);
+
+// '===' strict eqaulity, with type conversion
+console.log(stringFive === numberFive);
+console.log(stringFive !== numberFive);
+
+// object eqaulity by reference
+const effy1 = { name: "effy" };
+const effy2 = { name: "effy" };
+const effy3 = effy1;
+console.log(effy1 == effy2); //false
+console.log(effy1 === effy2); //false
+console.log(effy1 === effy3); //true
+
+// equality = puzzler
+console.log(0 == false);
+console.log(0 === false); // false with type strict equality
+console.log("" == false); // true
+console.log("" === false); // false
+console.log(null == undefined); // true
+console.log(null === undefined); // false
+
+// 8. Conditional operators: if
+// if, else if, else
+const guest = "coder";
+if (guest === "effy") {
+  console.log("Welcome, effy");
+} else if (guest === "coder") {
+  console.log("You are a amazing coder!");
+} else {
+  console.log(unknown);
+}
+
+// 9. Ternary(삼항, 3개) operator: ?
+// condition ? value1 : value2;
+console.log(guest === "effy" ? "yes" : "no");
+
+// 10. Switch statement
+// use for multiple if checks
+// use for enum(열거형)-like value check
+// use for multiple type checks in TS
+const browser = "IE";
+switch (browser) {
+    case "IE":
+        console.log("Go Away!");
+        break;
+    case "Chrome":
+    case "Firefox":
+        console.log("Love you");
+        break;
+    default:
+        console.log("Love you");
+        break;
+}
+
+// 11. Loops
+// while loop, while the condition is truthy,
+// body code is executed.
+let i = 3;
+while (i > 0) {
+    console.log(`while: ${i}`);
+    i--;
+}
+
+// do while loop, body code is executed first,
+// then check the condition. 
+// > 블럭을 먼저 실행 후  조건이 맞는지 안 맞는지 확인
+let j = 0;
+do {
+    console.log(`do while: ${i}`);
+    j--;
+} while (j > 0); // 무조건 조건 한번 실행.
+
+// for loop, for(begin; condition; step)
+for (i = 3; i > 0; i--){
+    console.log(`for: ${i}`);
+}
+for (let i = 3; i > 0; i = i - 2){
+    // inline variable declaration
+    console.log(`inline variable for: ${i}`);
+}
+
+// nestend loops
+for (let i = 0; i < 10; i++){
+    for (let j = 0; j < 10; j++){
+        console.log(`i: ${i}, j:${j}`);
+    }
+}
+
+// break, continue
+// Q1. iterate from 0 to 10 and print only even numbers (use continue)
+for (let i = 0; i <= 11; i++){
+    if (i % 2 === 0) {
+        console.log(`q1 : ${i}`);
+    } else {
+        continue;
+    }
+}
+
+// Q2. iterate from 0 to 10 and print numbers until reaching 8(use break)
+for (let i = 0; i < 10; i++){
+    if (i > 8) {
+        break;
+    }
+    console.log(`q2 : ${i}`);
+}
---
+++ variable.js
@@ -0,0 +1,123 @@
+// Data Types
+
+// 1. Use strict
+// added in ES 5
+// use this for Vanila Javascript.
+"use strict";
+
+// 2. Variable, rw(read/write)
+// * let (added in ES6)
+// global & block Scope
+let globalName = "It's global name";
+{
+    let name = "effy";
+    console.log(name);
+    name = "change the name";
+    console.log(name);
+    console.log(globalName);
+}
+console.log(name);
+console.log(globalName);
+
+// * var (don't ever use this!)
+// var hoisting : move declaration from bottom to top
+// --> hoising은 어디에 선언했는지와 상관없이 제일 상단으로 선언을 끌어올려주는 것.
+// has no block scope (block을 철저히 무시)
+{
+    age = 4;
+    var age;
+}
+console.log(age); // block scope 무시.
+
+// 3. Constant, r(read only) 
+// use 'const' whenever possible!
+// only use 'let' if variable needs to change.
+// 한번 선언(할당) 시 값이 절대 바뀌지 않음. 
+
+// favor immutable data type always for a few reasons :
+// - secutiry
+// - thread safety
+// - reduce human mistakes
+const daysInWeek = 7;
+const maxNumber = 5;
+
+// * Note!
+// Immutable data types: primitive types, frozen objects (i.e. object.freeze())
+// Mutable data types: all objects by default are mutable in JS
+
+// 4. Variable types
+// primitive (single items) : number, string, boolean, null, undefined,symbol
+// object (box container)
+// function (first-class function)
+const count = 17; //integer
+const size = 17.1; //decimal number
+console.log(`value: ${count} , type: ${typeof count}`);
+console.log(`value: ${size}, type: ${typeof size}`);
+
+// Number - special numeric values: Infinity, -infinity, Nan
+const infinity = 1 / 0;
+const negativeInfinity = -1 / 0;
+const nAn = 'not a number' / 2;
+console.log(infinity);
+console.log(negativeInfinity);
+console.log(nAn);
+
+// bigInt (fairly new, don't use it yet)
+const bigInt = 1241341234123412341234n;
+console.log(`value: ${bigInt}, type:${typeof bigInt}`);
+
+// String
+const char = 'c';
+const brendan = 'brendan';
+const greeting = 'hello ' + brendan; // string과 변수
+console.log(`value: ${greeting}, type: ${typeof greeting}`);
+// template literals(string) : use backtick(`)
+const helloBob = `hi ${brendan}!`; 
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+
+// Boolean
+// false : 0, null, undefined, null, NaN, ''
+// true : any other value
+const canRead = true;
+const test = 3 < 1; //false
+console.log(`value: ${canRead}, type: ${typeof canRead}`);
+console.log(`value: ${test}, type: ${typeof test}`);
+
+// null
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`);
+
+// undefined
+let x; // 값을 지정하지 않음
+let xx = undefined;
+console.log(`value: ${x}, type: ${typeof x}`);
+console.log(`value: ${xx}, type: ${typeof xx}`);
+
+// symbol, create unique indentifiers for objects
+const symbol1 = Symbol('id');
+const symbol2 = Symbol("id");
+// > 두가지의 심볼은 다름. 동일한 string을 작성했어도 고유한 식별자로 생성.
+console.log(symbol1 === symbol2); //false
+const gSymbol1 = Symbol.for('id');
+const gSymbol2 = Symbol.for("id");
+console.log(gSymbol1 === gSymbol2); //true
+
+// object, real-life object, data structure
+const effy = { name: 'effy', age: '27' };
+console.log(effy);
+effy.name = "saeyoung";
+console.log(effy); //name: 'saeyoung', age: '27'
+
+// 5. Dynamic typing : dynamically typed language
+let text = 'hello';
+console.log(text.charAt(0)); //h
+console.log(`value: ${text}, type: ${typeof text}`); //srting
+text = 1;
+console.log(`value: ${text}, type: ${typeof text}`); //number
+text = '7' + 1;
+console.log(`value: ${text}, type: ${typeof text}`); //string
+text = '6' / '2';
+console.log(`value: ${text}, type: ${typeof text}`); //number
+console.log(text.charAt(0)); //error
+
+
