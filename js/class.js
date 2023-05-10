82604a411950308f8e132bfb7cad482a4d792501
saeyoung choi <saeyoungchoi@Salin-Saeyoung-iMac.local>
2023-05-10 17:39:17 +0900

    .

--- js/class.js
+++ js/class.js
@@ -119,3 +119,24 @@ console.log(triangle instanceof Rectangle);
 console.log(triangle instanceof Triangle);
 console.log(triangle instanceof Shape);
 console.log(triangle instanceof Object);
+
+// QUIZ : function caculate(command, a, b)
+// command : add, substract, divide, multiply, remainder
+
+function calculate(command, a, b) {
+  switch (command) {
+    case "add":
+      return a + b;
+    case "substract":
+      return a - b;
+    case "divide":
+      return a / b;
+    case "multiply":
+      return a * b;
+    case "remainder":
+      return a % b;
+    default:
+      throw Error("unknown command");
+  }
+}
+console.log(calculate("add", 3, 4));
