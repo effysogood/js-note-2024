a9ebac749dc131656cfb720c5f1bedf682ec80da
sy.choi <sy.choi@salin.co.kr>
2023-07-13 16:21:34 +0900

    feat: operators and comments

---
+++ 2.error-hadling.js
@@ -0,0 +1,24 @@
+// try catch finally
+
+function readFile(path) {
+    throw new Error('파일 경로를 찾을 수 없음');
+    return '파일의 내용';
+}
+
+function processFile(path) {
+    let content;
+    try {
+        // 함수 호출
+        content = readFile(path);
+    } catch (error) {
+        console.log(error);
+        content = 'Default';
+    } finally {
+        console.log('성공하든 실패하든 마지막으로 리소스를 정리할 수 있음!');
+    }
+    const result = `hi, ${content}`;
+    return result;
+}
+
+const result = processFile('경로');
+console.log(result);
